fb24fa7f50b8aed84a70792b4e5993350fdcd9d2
rychnav <rychnavska.alena@gmail.com>
2021-03-09 20:13:28 +0100

    Compile Sass

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,8 @@
  * (and its CSS file) in your base layout (`base.html.twig`).
  */
 
-// Any CSS you import will output into a single CSS file (`app.css` in this case).
-import './styles/app.css';
+// Any CSS you import will output into a single CSS file (`app.scss` in this case).
+import './styles/app.scss';
 
 // Start the Stimulus application.
 import './bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. `app.js`)
-     * and one CSS file (e.g. `app.css`) if your JavaScript imports CSS.
+     * and one CSS file (e.g. `app.scss`) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
 
@@ -55,7 +55,7 @@ Encore
     })
 
     // Enables Sass/SCSS support.
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // Uncomment if you use TypeScript.
     //.enableTypeScriptLoader()
