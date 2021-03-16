2e9d517cbfea4615d962c6595804508ed6b8664c
rychnav <rychnavska.alena@gmail.com>
2021-03-16 21:01:07 +0100

    Generate User fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Tests\Factory\UserFixtureFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        UserFixtureFactory::new()->asUser()->afterInstantiate(function(User $object) {
+            $password = $this->passwordEncoder->encodePassword($object, 'kitten');
+            $object->setPassword($password);
+        })->createMany(90);
+
+        UserFixtureFactory::new()->asAdmin()->afterInstantiate(function(User $object) {
+            $password = $this->passwordEncoder->encodePassword($object, 'kitten');
+            $object->setPassword($password);
+        })->createMany(10);
     }
 }
---
+++ tests/Factory/UserFixtureFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static User|Proxy findOrCreate(array $attributes)
+ * @method static User|Proxy random()
+ * @method static User[]|Proxy[] randomSet(int $number)
+ * @method static User[]|Proxy[] randomRange(int $min, int $max)
+ * @method static UserRepository|RepositoryProxy repository()
+ * @method User|Proxy create($attributes = [])
+ * @method User[]|Proxy[] createMany(int $number, $attributes = [])
+ */
+class UserFixtureFactory extends ModelFactory
+{
+    protected function getDefaults(): array
+    {
+        return [
+            'email' => self::faker()->unique()->email,
+        ];
+    }
+
+    public function asAdmin(): self
+    {
+        return $this->addState(['roles' => ['ROLE_ADMIN']]);
+    }
+
+    public function asUser(): self
+    {
+        return $this->addState(['roles' => ['ROLE_USER']]);
+    }
+
+    protected function initialize(): self
+    {
+        // See: https://github.com/zenstruck/foundry#initialization.
+        return $this
+            //->beforeInstantiate(function(User $user) {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return User::class;
+    }
+}
